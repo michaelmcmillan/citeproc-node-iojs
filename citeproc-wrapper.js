15a4c8d8179fd783963554b43cb9688096b65ffd
Michael McMillan <email@michaelmcmillan.net>
2015-04-21 14:56:26 +0200

    added a simple wrapper for the citeproc-js library

---
+++ citeproc-wrapper.js
@@ -0,0 +1,86 @@
+var fs        = require('fs');
+var assert    = require('assert');
+var CSL       = require('./src/citeproc.js').CSL;
+
+function Citeproc (styleLocation, localeLocation, done) {
+    
+    // Constructs the wrapper
+    this.construct = function () {
+        var self = this;
+
+        self.loadStyle(styleLocation, function () {
+            self.loadLocale(localeLocation, function () {
+                self.setupSys();
+                citeproc = new CSL.Engine(sys, style);
+                done();
+            });
+        });
+    }
+
+    // The internal CSL.Engine object 
+    var citeproc;
+
+    // Implements retrieveLocale and retrieveItem
+    var sys;
+
+    // Holds the content of a .csl file 
+    var style;
+    
+    // All the added references which will be formatted
+    var citations = [];
+    
+    // Holds the content of a locales-*-*.xml file
+    var locale;
+
+    // Setter for citation array
+    this.setCitations = function (addedCitations) {
+        citations = addedCitations;
+    }
+
+    // Rigs up the sys object for the internal citeproc
+    this.setupSys = function () {
+        sys = {
+            retrieveLocale: function (language) {
+                return locale;
+            },
+            retrieveItem: function (id) {
+                return citations[id];
+             }
+        };
+    }
+
+    // Generates and returns the bibliography 
+    this.makeBibliography = function () {
+        citeproc = new CSL.Engine(sys, style);
+        citeproc.updateItems(['Item-1','Item-2','Item-3','Item-4']);
+        return citeproc.makeBibliography();
+    }
+
+    this.loadLocale = function (file, done) {
+        fs.readFile(file, function (err, data) {
+            if (err) return done(err);
+            locale = data.toString();
+            return done(); 
+        });
+    }
+
+    this.loadStyle = function (file, done) {
+        fs.readFile(file, function (err, data) {
+            if (err) return done(err);
+            style = data.toString();
+            return done(); 
+        });
+    }
+
+    this.construct();
+}
+
+var style  = './styles/chicago-fullnote-bibliography.csl';
+var locale = './locales/locales-en-US.xml';
+
+var cite   = new Citeproc(style, locale, function () {
+    cite.setCitations(require('./test/citations.json'));
+    console.log(cite.makeBibliography()); 
+});
+
+module.exports = Citeproc;
