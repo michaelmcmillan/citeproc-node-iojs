52b69d00b167858a523c256e81320e0191bc36f3
Michael McMillan <email@michaelmcmillan.net>
2015-04-21 15:28:02 +0200

    updated test to use wrapper

--- test/test.js
+++ test/test.js
@@ -1,28 +1,18 @@
-var fs        = require('fs');
 var assert    = require('assert');
-var CSL       = require('../src/citeproc.js').CSL;
 var citations = require('./citations.json');
+var Citeproc  = require('../citeproc-wrapper.js');
 
 describe('citeproc-js', function () {
     
-    it('should parse a JSON object to a correct bibliography', function () {
+    it('should parse a JSON object to a correct bibliography', function (done) {
+        this.timeout(3000);
 
-        var sys = {
-            retrieveLocale: function (lang) {
-                return fs.readFileSync('./locales/locales-en-US.xml').toString();
-            },
+        var style  = './styles/chicago-fullnote-bibliography.csl';
+        var locale = './locales/locales-en-US.xml';
 
-            retrieveItem: function (id) {
-                return citations[id];
-            }
-        }
-
-        var xml = fs.readFileSync('./styles/chicago-fullnote-bibliography.csl').toString();
-        var citeproc = new CSL.Engine(sys, xml);
-
-        citeproc.updateItems(['Item-1','Item-2','Item-3','Item-4']);
-        var result = citeproc.makeBibliography();
-        
-        assert.equal(result[1].join('\n').length > 100, true);
+        var cite   = new Citeproc(style, locale, function () {
+            cite.setCitations(require('./citations.json'));
+            done();
+        });
     });
 });
